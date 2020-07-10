089ad90cf01de7a4066ca22e6261f462dd714d1d
Pavel Chekanouski <PavelChekanousli@gmail.com>
2020-07-10 07:36:33 +0300

    Fixed issue with File, also made CaesorCode class with needed methods. Added Decryption functionality.

--- main.rb
+++ main.rb
@@ -3,7 +3,9 @@ puts "Hi, enter number of shifts. The number makes sense, if it's from 1 to 25."
 shift = gets.to_i
 begin
   if(shift > 0 && shift < 26)
-    encrypt(shift)
+    shifr = CaesarCode.new(shift)
+    shifr.encrypt
+    shifr.decrypt
   else
     raise Exception
   end
--- сaesarCode.rb
+++ сaesarCode.rb
@@ -1,13 +1,40 @@
-def encrypt(shift)
+class CaesarCode
+  
+  @shift
+  
+  def initialize(shift)
+    @shift = shift
+  end
+
+  def get_shift
+    @shift
+  end
+
+  def decrypt()
+    letters = File.new("encrypted_text.txt", "r:UTF-8").read.chars
+    letters = letters.map do |x|
+      case x.ord
+        when 65..90 then ((x.ord - self.get_shift) > 64 ? (x.ord - self.get_shift) : (x.ord - self.get_shift) + 26).chr
+        when 97..122 then ((x.ord - self.get_shift) > 96 ? (x.ord - self.get_shift) : (x.ord - self.get_shift) + 26).chr
+        when " ".ord then x
+        when "\n".ord then x
+      end
+    end  
+    File.open("decrypted_text.txt", "w:UTF-8") { |file| file.write(letters.join()) }
+    puts "Decryption done"
+  end
+
+  def encrypt()
   letters = File.new("text.txt", "r:UTF-8").read.chars
   letters = letters.map do |x| 
     if(x != " " && x != "\n")
-      ((x.ord + shift) < 123 ? (x.ord + shift) : (x.ord + shift) - 26).chr
+      ((x.ord + self.get_shift) < 123 ? (x.ord + self.get_shift) : (x.ord + self.get_shift) - 26).chr
     else
       x
     end
   end  
-File.open(File.new("output.txt", "w:UTF-8"), 'w') { |file| file.write(letters.join()) }
-puts "Well done"
-end
+  File.open("encrypted_text.txt", "w:UTF-8") { |file| file.write(letters.join()) }
+  puts "Encryption done"
+  end
 
+end
